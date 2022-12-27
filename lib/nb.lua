3d267a58a45ea76eb48452135faf2368e07391f6
Naomi Seyfer <naomi@seyfer.org>
2022-12-27 05:36:55 +0000

    Add midi to players by default

--- lib/nb.lua
+++ lib/nb.lua
@@ -12,9 +12,41 @@ nb.players = note_players -- alias the global here. Helps with standalone use.
 
 nb.none = player_lib:new()
 
+local function add_midi_players()
+    for i, v in ipairs(midi.vports) do
+        if v.connected then
+            local conn = midi.connect(i)
+            local player = {
+                conn = conn
+            }
+            function player:add_params()
+                params:add_group("midi_voice_"..i, "midi: "..v.name, 1)
+                params:add_number("midi_chan_"..i, "channel", 1, 16)
+                params:hide("midi_voice_"..i)
+            end
+            function player:note_on(note, vel)
+                self.conn:note_on(note, util.clamp(math.floor(127*vel), 0, 127))
+            end
+            function player:note_off(note)
+                self.conn:note_off(note)
+            end
+            function player:active()
+                params:show("midi_voice_"..i)
+                _menu.rebuild_params()
+            end
+            function player:inactive()
+                params:hide("midi_voice_"..i)
+                _menu.rebuild_params()
+            end
+            nb.players[v.name] = player
+        end
+    end
+end
+
 -- Call from your init method.
 function nb:init()
-    refcounts = {}
+    nb_player_refcounts = {}
+    add_midi_players()
     nb_params_added = false
     self:stop_all()
 end
